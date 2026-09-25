13726c5c30677e40099eb8c458930f960b60a0b0
KT Serwis Komputerowy <57107483+kamillo2000@users.noreply.github.com>
2026-09-25 19:23:41 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,11 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://xenly.cloud",
-      lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
